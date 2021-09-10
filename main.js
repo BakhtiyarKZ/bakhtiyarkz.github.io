dd9185264aedaaad4ab81949b953e5870c39eee5
BakhtiyarKZ <48028890+BakhtiyarKZ@users.noreply.github.com>
2021-09-10 23:24:14 +0600

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,8 @@ getNFTs = async () => {
                 let metadata = JSON.parse(n.metadata);
                 let content = `
                       <div class="card col-md-3">
-                     <img src="${metadata.animation_url}" class="card-img-top" alt="...">
+                     
+                     <video src="${metadata.animation_url}" autoplay poster="logo.png"></video>
                      <div class="card-body">
                      <h5 class="card-title"> ${metadata.name} </h5>
                      <p class="card-text">${metadata.description}</p>
